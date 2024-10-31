8cdef809bb1d5bd9685952cc9d091e98bacb0376
Rehma Fatmi <rehmafatmi@gmail.com>
2024-10-31 23:48:00 +0500

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, isPrime;
+
+    printf("Enter the range (start and end): ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+    for (int num = start; num <= end; num++) {
+        if (num < 2) continue;
+        isPrime = 1;
+        
+        
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+        
+        if (isPrime) {
+            printf("%d ", num);
+        }
+    }
+    printf("\n");
+    return 0;
+}
